932f9242e0d40f1f33453a445fd904aca5a4f098
akimyou <akimyou@icloud.com>
2023-04-23 15:48:28 +0800

    up

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,33 +1,125 @@
 <template>
-  <el-form :model="form" label-width="120px">
-    <el-form-item label="InputDemo">
-      <InputDemo v-model="form.inputDemoValue" />
+  <el-form
+    ref="ruleFormRef"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="120px"
+    class="demo-ruleForm"
+  >
+    <el-form-item label="userName" prop="userName">
+      <!-- <el-input :model-value="ruleForm.userName" @update:model-value="modelChange" /> -->
+      <InputDemo :model-value="ruleForm.userName" @update:model-value="modelChange"></InputDemo>
     </el-form-item>
-    <el-form-item label="InputDemo2">
-      <InputDemo v-model="form.inputDemoValue2" />
+    <el-form-item label="Password" prop="pass">
+      <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
     </el-form-item>
-    <el-form-item label="InputDemo3">
-      <InputDemo v-model="form.inputDemoValue3" />
+    <el-form-item label="Confirm" prop="checkPass">
+      <el-input
+        v-model="ruleForm.checkPass"
+        type="password"
+        autocomplete="off"
+      />
+    </el-form-item>
+    <el-form-item label="Age" prop="age">
+      <el-input v-model.number="ruleForm.age" />
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="onSubmit">Create</el-button>
-      <el-button>Cancel</el-button>
+      <el-button type="primary" @click="submitForm(ruleFormRef)"
+        >Submit</el-button
+      >
+      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
     </el-form-item>
   </el-form>
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
 import InputDemo from './components/InputDemo.vue'
 
-// do not use same name with ref
-const form = reactive({
-  inputDemoValue: 'inputDemoValue',
-  inputDemoValue2: 'inputDemoValue2',
-  inputDemoValue3: 'inputDemoValue3',
+const ruleFormRef = ref<FormInstance>()
+
+const checkAge = (rule: any, value: any, callback: any) => {
+  if (!value) {
+    return callback(new Error('Please input the age'))
+  }
+  setTimeout(() => {
+    if (!Number.isInteger(value)) {
+      callback(new Error('Please input digits'))
+    } else {
+      if (value < 18) {
+        callback(new Error('Age must be greater than 18'))
+      } else {
+        callback()
+      }
+    }
+  }, 1000)
+}
+
+const validateUserName = (rule: any, value: any, callback: any) => {
+  console.log('validateUserName', value)
+  if (value === '') {
+    callback(new Error('Please input the UserName'))
+  } else {
+    callback()
+  }
+}
+
+const validatePass = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('Please input the password'))
+  } else {
+    if (ruleForm.checkPass !== '') {
+      if (!ruleFormRef.value) return
+      ruleFormRef.value.validateField('checkPass', () => null)
+    }
+    callback()
+  }
+}
+const validatePass2 = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('Please input the password again'))
+  } else if (value !== ruleForm.pass) {
+    callback(new Error("Two inputs don't match!"))
+  } else {
+    callback()
+  }
+}
+
+const ruleForm = reactive({
+  userName: 'xxxx',
+  pass: '',
+  checkPass: '',
+  age: '',
 })
 
-const onSubmit = () => {
-  console.log('submit!', form)
+const rules = reactive<FormRules>({
+  userName: [{ validator: validateUserName, trigger: 'change' }],
+  pass: [{ validator: validatePass, trigger: 'blur' }],
+  checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+  age: [{ validator: checkAge, trigger: 'blur' }],
+})
+
+const modelChange = (val: string) => {
+  console.log('here', val)
+  ruleForm.userName  = val
+}
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!')
+      return false
+    }
+  })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
 }
 </script>
